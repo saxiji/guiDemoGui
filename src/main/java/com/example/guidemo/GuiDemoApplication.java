6b59326163bbc15d04cc7a9273cab0cf9771ad6a
306491910@qq.com <306491910@qq.com>
2021-11-11 17:26:09 +0800

    Initial commit

---
+++ src/main/java/com/example/guidemo/GuiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.guidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/guidemo/GuiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
