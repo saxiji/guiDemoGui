548b5a2c3f64026d180974fadd803767c900f746
306491910@qq.com <306491910@qq.com>
2021-11-11 17:56:27 +0800

    第一次提交

--- src/main/java/com/example/guidemo/GuiDemoApplication.java
+++ src/main/java/com/example/guidemo/GuiDemoApplication.java
@@ -1,13 +1,41 @@
 package com.example.guidemo;
 
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
-public class GuiDemoApplication {
+public class GuiDemoApplication extends Application {
+
+    private static ConfigurableApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        SpringApplication.run(GuiDemoApplication.class, args);
+        applicationContext = SpringApplication.run(GuiDemoApplication.class, args);
+        Application.launch(GuiDemoApplication.class,args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("my test window");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(500);
+        primaryStage.setScene(new Scene(createRoot()));
+
+        primaryStage.show();
+    }
+
+    private Parent createRoot() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(GuiDemoApplication.class.getResource("/main.fxml"));
+        fxmlLoader.setControllerFactory(applicationContext::getBean);
+        return fxmlLoader.load();
+    }
 }
